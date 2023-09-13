dc12ac722caf057096dce8e27721ecbbadfc27a2
rohithakadali <rohithakadali0@gmail.com>
2023-09-13 22:51:26 +0530

    project completed uploadning deployment

---
+++ dashboards/forms.py
@@ -0,0 +1,22 @@
+from blog.models import Blog, Category, User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model=Category
+        fields='__all__'
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model=Blog
+        fields=('title','category','featured_image','short_description','blog_body','status','is_featured')
+class AddUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=('username','email','first_name','last_name','is_active','is_superuser','groups','user_permissions')
+class EditUserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=('username','email','first_name','last_name', 'is_active','is_superuser','groups','user_permissions')
---
+++ social/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from social.models import About,SocialLink
+
+admin.site.register(About)
+admin.site.register(SocialLink)
